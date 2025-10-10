e1cbb55c3ba412b6c7f59ed0f818bdd1bd6a192b
bouguerra yassine <bouguerra.yassine@esprit.tn>
2025-10-10 10:26:19 +0100

    dexieme commit validators

--- AI2/ConferenceApp/models.py
+++ AI2/ConferenceApp/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator
 from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+import string
+import random
+from django.utils import timezone
+
 # Create your models here.
+def generate_submission_id():
+    letters = ''.join(random.choices(string.ascii_uppercase, k=8))
+    return f"SUB{letters}"
+def validate_keywords(value):
+    # Sépare les mots par virgules et supprime les espaces inutiles
+    keywords = [k.strip() for k in value.split(',') if k.strip()]
+    if len(keywords) > 10:
+        raise ValidationError("You can specify a maximum of 10 keywords separated by commas.")
+def validate_keywords(value):
+    # Sépare les mots par virgules et supprime les espaces inutiles
+        keywords = [k.strip() for k in value.split(',') if k.strip()]
+        if len(keywords) > 10:
+            raise ValidationError("You can specify a maximum of 10 keywords separated by commas.")    
 class CONFERENCE(models.Model):
     conference_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -19,16 +37,19 @@ class CONFERENCE(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def clean(self):
-        if self.start_date > self.end_date:
-            raise ValidationError("Start date must be before end date.")    
+        if self.start_date and self.end_date:
+            if self.start_date > self.end_date:
+                raise ValidationError("Start date must be before end date.")    
+        
+    
 
 
 class SUBMISSION(models.Model):
-    submission_id = models.CharField(max_length=255,primary_key=True,unique=True)
+    submission_id = models.CharField(max_length=255,primary_key=True,unique=True,editable=False,default=generate_submission_id)
     title = models.CharField(max_length=255)
     abstract = models.TextField()
-    keywords = models.TextField()
-    papers = models.FileField(upload_to='paper/')
+    keywords = models.TextField(validators=[validate_keywords])
+    papers = models.FileField(upload_to='paper/',validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
     STATUS =[
         ('submitted','submitted'),
         ('under review', 'under review'),
@@ -42,3 +63,7 @@ class SUBMISSION(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey('UserApp.USER',on_delete=models.CASCADE,related_name='submissions')
     conference = models.ForeignKey(CONFERENCE,on_delete=models.CASCADE,related_name='submissions')  
+    def clean(self):
+        if self.submission_date:
+            if self.conference.start_date < timezone.now().date() and self:
+                raise ValidationError("The conference has already started.")
--- AI2/SessionApp/models.py
+++ AI2/SessionApp/models.py
@@ -1,7 +1,15 @@
 from django.db import models
-from ConferenceApp.models import CONFERENCE
+from ConferenceApp.models import *
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 # Create your models here.
+def verif_start_end_time(self):
+    if self.start_time > self.end_time:
+        raise ValidationError("Start time must be before end time.")
+
+     
+     
 class SESSION(models.Model):
     session_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
@@ -9,10 +17,15 @@ class SESSION(models.Model):
     session_day = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()   
-    room = models.CharField(max_length=255)
+    room = models.CharField(max_length=255,validators=[RegexValidator(regex=r'^[A-Za-z0-9\s]+$', message="Room name must contain only letters and numbers (no special characters).")])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     conference = models.ForeignKey(CONFERENCE,on_delete=models.CASCADE,related_name="sessions")
-
-
+    def clean(self):
+        if self.conference:
+            if not (self.conference.start_date <= self.session_day<= self.conference.end_date):
+                        raise ValidationError({"session_day": "Session day must be between conference start date and end date."})
+            self.verif_start_end_time()
+ 
+       
     
